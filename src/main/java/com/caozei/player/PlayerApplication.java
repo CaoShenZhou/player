dca5fe8abacf2fa2accc1ad925b1ef6a0f9beda3
caoshenzhou <1791915772@qq.com>
2021-05-18 22:02:46 +0800

    首次提交

---
+++ src/main/java/com/caozei/player/PlayerApplication.java
@@ -0,0 +1,13 @@
+package com.caozei.player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/caozei/player/PlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caozei.player;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
