e2c95154d583cc457e054fa3ac1ffee877b8876d
caoshenzhou <1791915772@qq.com>
2021-06-04 01:48:19 +0800

    公共字段自动填充配置

---
+++ src/main/java/com/shenzhou/player/config/MyMetaObjectHandler.java
@@ -0,0 +1,27 @@
+package com.shenzhou.player.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * 公共字段自动填充配置
+ *
+ * @author Mr.Cao
+ **/
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
+        this.strictInsertFill(metaObject, "updateTime", Date.class, new Date());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "updateTime", Date.class, new Date());
+    }
+}
